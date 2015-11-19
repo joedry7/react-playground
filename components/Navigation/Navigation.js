477b5ac92acd6976c90bbdb38333a66aedb244a3
Jordan Sissel <jls@semicomplete.com>
2015-11-18 22:31:52 -0800

    Explore using material-ui

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -3,22 +3,32 @@
  * https://github.com/koistya/react-static-boilerplate
  * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
  */
+/* Changes copyright (c) Jordan Sissel */
 
-import React from 'react';
 import './Navigation.scss';
 import Link from '../Link';
+import AppBar from 'material-ui/lib/app-bar';
+import LeftNav from 'material-ui/lib/left-nav';
+import IconButton from 'material-ui/lib/icon-button';
 
-function Navigation() {
-  return (
-    <ul className="Navigation" role="menu">
-      <li className="Navigation-item">
-        <a className="Navigation-link" href="/" onClick={Link.handleClick}>Home</a>
-      </li>
-      <li className="Navigation-item">
-        <a className="Navigation-link" href="/about" onClick={Link.handleClick}>About</a>
-      </li>
-    </ul>
-  );
+import React, { Component } from 'react';
+
+export default class extends Component {
+  toggleNav() {
+    this.refs.leftNav.toggle();
+  }
+
+  render() {
+    let menuItems = [
+      { 
+        text: <span>test<b>hello</b></span>,
+        onToggle: function() { alert("OK") },
+      }
+    ]
+    return <div>
+      <AppBar title="hello"
+        onLeftIconButtonTouchTap={this.toggleNav.bind(this)} />
+      <LeftNav ref="leftNav" docked={false} menuItems={menuItems} />
+    </div>
+  }
 }
-
-export default Navigation;
